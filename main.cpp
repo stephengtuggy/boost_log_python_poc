042fc414c9b9e2e900d11b3a0a332282024af1bd
Stephen G Tuggy <sgt@stephengtuggy.com>
2023-03-31 13:08:52 -0700

    Add vcpkg.json, Python stuff, etc.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <boost/python.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Py_Initialize();
+
+    try {
+        boost::python::object main_module = boost::python::import("__main__");
+        boost::python::object main_namespace = main_module.attr("__dict__");
+        boost::python::object ignored = boost::python::exec("print(\"Hello world from Python!\")", main_namespace);
+    } catch (const boost::python::error_already_set&) {
+        PyErr_Print();
+        return 1;
+    }
+
     return 0;
 }
